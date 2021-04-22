0ef89135d536cd1c3ee93bee31108012bba9b0b2
Papiex <boutdepapier27@outlook.fr>
2021-04-22 10:26:24 +0200

    code_loading_functions => code_load

--- code_load.py
+++ code_load.py
@@ -1,27 +1,27 @@
-from extracting_functions import search_books_links_cat, picture_download
-from transformation_functions import find_info_book
+from extract import search_books_links_category, download_picture
+from transform import find_info_book
 
 import csv
 import os
 import time
 
 
-def make_dir_category(name_cat):
+def make_directory_category(name_category: str):
     """Make directories => csv files/name_cat/jpg files/"""
 
     try:
-        os.makedirs("csv files/" + name_cat + "/jpg files/")
+        os.makedirs("csv files/" + name_category + "/jpg files/")
 
     except BaseException:
         print("Error in creation of directories")
         pass
 
 
-def writing_to_csv(name_cat):
+def writing_to_csv(name_category: str):
     """with name_csv as name, writing a csv in writing mode"""
 
-    print("Writing of " + name_cat + ".csv and downloading pictures")
-    with open("csv files/" + name_cat + "/" + name_cat + ".csv", "w", encoding="utf-8") as f:
+    print("Writing of " + name_category.upper() + ".CSV and downloading pictures")
+    with open("csv files/" + name_category + "/" + name_category + ".csv", "w", encoding="utf-8") as f:
 
         colums_name = [
             "[product_page_url]",
@@ -41,30 +41,30 @@ def writing_to_csv(name_cat):
         writer.writeheader()
 
 
-def append_to_csv(cat_names, name_cat):
-    """call the function search_books_links_cat() who go search for all links in the dict {cat_names} with the key variable (name_cat)
+def append_to_csv(category_names_dict: dict[str], name_category: str):
+    """
+    call the function search_books_links_category() who go search for all links in the dict {category_names_dict} with the key variable (name_category)
     and return a list [book_links] and for all url in, we call the function find_info_book() who return "book info" in a list [info] and write this list in append mode to the
-    current key variable (name_cat".csv") """
-
-    # transform variable number in to global variable
-    # and for each function call, reset value of number to "1"
-    global number
-    number = 1
+    current key variable (name_category".csv")
+    """
+    
+    number: int = 1
     book_link_count = 0
     t0 = time.time()
-    # call the function search_books_links_cat() for each url in  {dict}=>cat_names(key)=>[names_cat]<=[list]
+
+    # call the function search_books_links_cat() for each url in  {dict}=>category_names_dict(key)=>[names_category]<=[list]
     # all books links in will be stored in [books_links]
-    for link in cat_names[name_cat]:
-        books_links = search_books_links_cat(link)
+    for link in category_names_dict[name_category]:
+        books_links = search_books_links_category(link)
+
         # Extract info book for each url in [book_links]
         for book_link in books_links:
             info = find_info_book(book_link)
-            picture_download(info[7], name_cat, number)
+            download_picture(info[7], name_category, number)
             # print the progress of books/ picture downloaded
-            print(str(book_link_count) + " writed, " +
-                  str(number - 1) + " downloaded", end="\r")
+            print(str(book_link_count) + " writed/download...", end="\r")
 
-            with open("csv files/" + name_cat + "/" + name_cat + ".csv", "a", encoding="utf-8") as f:
+            with open("csv files/" + name_category + "/" + name_category + ".csv", "a", encoding="utf-8") as f:
                 colums_name = [
                     "[product_page_url]",
                     "[upc]",
@@ -97,16 +97,16 @@ def append_to_csv(cat_names, name_cat):
             book_link_count += 1
 
     t1 = time.time()
+    total = timer(t0, t1)
+    # When category is complete print the number of book in and pictures and time elapsed
+    print("Done ! " + str(book_link_count) + " Books/Pictures Writed/Download, " + total + (" seconds time elapsed !"))
+    print("=========================================================")
+
+
+def timer(t0: float, t1: float) -> str:
+    """calculate time elapsed with t1 - t0 and return total"""
+    
     total = t1 - t0
     total = round(total, 3)
-    # When category is complete print the number of book in and pictures and
-    # time elapsed
-    print("Done ! " +
-          str(book_link_count) +
-          " books info writed, " +
-          str(number -
-              1) +
-          " pictures downloaded, " +
-          str(total) +
-          (" seconds time elapsed !"))
-    print("=========================================================")
+    total = str(total)
+    return total
